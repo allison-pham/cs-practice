41c81b5a94e9acedca8acbdba2babc63f2b33d75
Allison <138368973+allison-pham@users.noreply.github.com>
2023-12-11 04:53:26 -0800

    Add palindrome problem

--- leetcode/java-level-easy/palindrome-number.java
+++ leetcode/java-level-easy/palindrome-number.java
@@ -17,5 +17,27 @@
 
 class Solution {
 
-  public boolean isPalindrome(int x) {}
+  public boolean isPalindrome(int x) {
+    String original = Integer.toString(x);
+
+    for (int i = 0; i < original.length() / 2; i++) {
+      if (original.charAt(i) != original.charAt(original.length() - 1 - i)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
+
+// for (int i = 0, j = original.length() - 1; i < j; i++, j--) {
+/*int i = x;
+    int reverse = 0;
+
+    while (i > 0) {
+      int last_num = i % 10;
+      reverse *= 10 + last_num
+      i /= 10;
+    }
+
+    return original == reversed;*/
