97a5aacae2fa18ebf9253657d8dd97c332b66258
Allison <138368973+allison-pham@users.noreply.github.com>
2023-12-29 12:23:47 -0800

    Solved remove element problem

--- leetcode/0000-0099/0027-remove-element/java-remove-element.java
+++ leetcode/0000-0099/0027-remove-element/java-remove-element.java
@@ -40,5 +40,16 @@
 
 class Solution {
 
-  public int removeElement(int[] nums, int val) {}
+  public int removeElement(int[] nums, int val) {
+    int ctr = 0; // Number of elements that aren't equal
+
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != val) {
+        nums[ctr] = nums[i];
+        ctr++;
+      }
+    }
+
+    return ctr;
+  }
 }
