c962e759259f6d796b4afee5432b57124ac90745
Allison <138368973+allison-pham@users.noreply.github.com>
2024-01-10 17:37:14 -0800

    Made adjustments to problem 1

--- cs-009b-python-ii/week-1-labs/problem-1-max-magnitude.py
+++ cs-009b-python-ii/week-1-labs/problem-1-max-magnitude.py
@@ -1,13 +1,22 @@
 # Transfer over to PyCharm
 
-def maxMagnitude(numList):
+def max_magnitude(num_list):
     # Given (a, b, c), find the max magnitude
-    maxVal = -1
+    max_val = -1
 
-    # for i in numList:
+    for i in num_list:
         # if negative: multiply by -1
-    
-    # for k in numList:
-        # if currentVal > maxVal: maxVal = currentVal
+        if i < 0:
+            i = i * -1
 
-    return maxVal
+    for k in num_list:
+        # if current_val > max_val: max_val = current_val
+        if k > max_val:
+            max_val = k
+
+    return max_val
+
+
+def main():
+    num_list = [1, 4, 7]
+    max_magnitude(num_list)
