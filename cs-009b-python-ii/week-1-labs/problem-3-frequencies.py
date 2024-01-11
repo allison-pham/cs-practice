949f430cdc070a69e0a6246e7d7b1a465c4f0a79
Allison <138368973+allison-pham@users.noreply.github.com>
2024-01-11 14:57:51 -0800

    Make minor adjustments

---
+++ cs-009b-python-ii/week-1-labs/problem-3-frequencies.py
@@ -0,0 +1,18 @@
+def frequencies(word_list):
+    # Track frequency of each individual word
+    # Create ctr for each individual word
+    # dictionary
+
+    frequency = {}
+
+    for word in word_list:
+        if word in frequency:
+            frequency[word] += 1
+        else:
+            frequency[word] = 1
+
+    return frequency
+
+
+word_list = ["hi", "hello", "hi"]
+print(frequencies(word_list))
