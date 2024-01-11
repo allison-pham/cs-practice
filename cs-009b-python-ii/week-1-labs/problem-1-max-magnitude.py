75e67600c44a126c71c3ea1946c0dde417c343b6
Allison <138368973+allison-pham@users.noreply.github.com>
2024-01-10 18:27:01 -0800

    Update code

--- cs-009b-python-ii/week-1-labs/problem-1-max-magnitude.py
+++ cs-009b-python-ii/week-1-labs/problem-1-max-magnitude.py
@@ -1,20 +1,22 @@
 def max_magnitude(num_list):
     # Given (a, b, c), find the max magnitude
-    max_val = -1
+    max_val = abs(num_list[0])
 
-    for i in num_list:
+    '''for i in num_list:
         # if negative: multiply by -1
         if i < 0:
-            i = i * -1
+            i = i * -1'''
 
     for k in num_list:
         # if current_val > max_val: max_val = current_val
-        if k > max_val:
-            max_val = k
+        if abs(k) > max_val:
+            max_val = abs(k)
 
     return max_val
 
 
 def main():
     num_list = [1, 4, 7]
-    max_magnitude(num_list)
+    print(max_magnitude(num_list))
+
+main()
